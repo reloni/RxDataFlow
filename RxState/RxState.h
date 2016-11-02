c9d82045c7b08a055d585fe9850b29ac618e6487
Anton Efimenko <reloni@ya.ru>
2016-11-02 21:23:32 +0300

    Initial Commit

---
+++ RxState/RxState.h
@@ -0,0 +1,19 @@
+//
+//  RxState.h
+//  RxState
+//
+//  Created by Anton Efimenko on 02.11.16.
+//  Copyright © 2016 Anton Efimenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxState.
+FOUNDATION_EXPORT double RxStateVersionNumber;
+
+//! Project version string for RxState.
+FOUNDATION_EXPORT const unsigned char RxStateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxState/PublicHeader.h>
+
+
