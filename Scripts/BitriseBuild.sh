8440af1c43157deea02ccf62281201a841621351
Anton Efimenko <reloni@ya.ru>
2018-02-24 20:50:37 +0300

    Update script

--- Scripts/BitriseBuild.sh
+++ Scripts/BitriseBuild.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-pip install --user awscli
-export PATH=$PATH:$HOME/.local/bin
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
+pip3 install awscli --upgrade --user
+aws --version
 
 sh Scripts/DownloadCacheFromS3.sh
